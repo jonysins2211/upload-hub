c77f349f41990c17eeb086fb78c4619e4495fa43
Subash Krishnan <88789785+subashbuilds@users.noreply.github.com>
2026-06-29 11:32:05 +0530

    Refactor upload_to_gofile to use MultipartWriter

--- uploaders/gofile.py
+++ uploaders/gofile.py
@@ -1,50 +1,80 @@
 import os
 import aiohttp
 
+from aiohttp import MultipartWriter
+
 from config import GOFILE_API_TOKEN
+from utils.upload_stream import UploadStream
 
 
-async def upload_to_gofile(file_path: str):
-
-    url = "https://upload.gofile.io/uploadfile"
+async def upload_to_gofile(
+    file_path,
+    status_message,
+    task_id,
+    keyboard
+):
 
     headers = {
         "Authorization": f"Bearer {GOFILE_API_TOKEN}"
     }
 
-    form = aiohttp.FormData()
+    stream = UploadStream(
+        file_path,
+        status_message,
+        task_id,
+        keyboard
+    )
 
-    with open(file_path, "rb") as file:
-        form.add_field(
-            "file",
-            file,
-            filename=os.path.basename(file_path),
-            content_type="application/octet-stream"
-        )
+    writer = MultipartWriter("form-data")
 
-        async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.post(url, data=form) as response:
+    part = writer.append(stream)
+
+    part.set_content_disposition(
+        "form-data",
+        name="file",
+        filename=os.path.basename(file_path)
+    )
+
+    part.headers["Content-Type"] = "application/octet-stream"
+
+    try:
+
+        async with aiohttp.ClientSession(
+            headers=headers
+        ) as session:
+
+            async with session.post(
+                "https://upload.gofile.io/uploadfile",
+                data=writer
+            ) as response:
 
                 text = await response.text()
-                
-                print(headers)
-
-                print("Status:", response.status)
-                print("Body:", text)
 
                 try:
                     data = await response.json(content_type=None)
                 except Exception:
                     raise Exception(
                         f"HTTP {response.status}\n\n{text}"
-                 )
+                    )
 
                 if response.status not in (200, 201):
                     raise Exception(
-                        data.get("message", f"HTTP {response.status}")
+                        data.get(
+                            "message",
+                            f"HTTP {response.status}"
+                        )
                     )
 
+    finally:
+
+        stream.close()
+
     if data.get("status") != "ok":
-        raise Exception(data.get("message", "Upload failed"))
+        raise Exception(
+            data.get(
+                "message",
+                "Upload failed"
+            )
+        )
 
     return data["data"]["downloadPage"]
